bb6c875d7b19437c0e97ff7fdbb5e432dc5e0fb1
Denis <beznikind@gmail.com>
2023-01-28 15:53:03 +0300

    Написать программу  для замены элементов на противоположные

---
+++ 002/Program.cs
@@ -0,0 +1,20 @@
+﻿// Написать программу  для замены элементов на противоположные
+
+void ArrayGange(int[] array)
+{
+    int length = array.Length;
+    for (int index = 0; index < length; index = index + 1)
+    {
+        array[index] = new Random().Next(-100 , 100);
+    }
+}
+int[] array = new int[20];
+ArrayGange(array);
+Console.WriteLine(string.Join("," , array ));
+
+int length = array.Length;
+for (int index = 0; index < length; index = index + 1 )
+{
+     array[index] = array[index] * -1 ;
+}
+Console.WriteLine(string.Join(",", array));
